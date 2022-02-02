5e084ce2f12134800e3144b7784828beaaa5eb12
Richard Evans <richard.evans@virtahealth.com>
2022-02-01 20:56:27 -0500

    add base

---
+++ libs/otherlib/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'otherlib',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/otherlib',
+};
---
+++ libs/otherlib/src/lib/otherlib.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface OtherlibProps {}
+
+export function Otherlib(props: OtherlibProps) {
+  return (
+    <div>
+      <h1>Welcome to Otherlib!</h1>
+    </div>
+  );
+}
+
+export default Otherlib;
